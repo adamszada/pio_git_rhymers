c5c96d52903a67a813fb437e51b1e5230ad71ede
adamszada <235985@edu.p.lodz.pl>
2022-05-11 13:27:38 +0200

    Poprawa zadania 1.4.3 + Dodanie 1.5.5 hermetyzacja nieprywatnych atrybutów Dodanie diagramu klas UML

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,17 +2,21 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-	public static final int DEFAULT_RETURN = -1;
+	private static final int DEFAULT_RETURN = -1;
 	private Node last;
-	protected int i;
+	private int i;
+
+	public static int getDefaultReturn() {
+		return DEFAULT_RETURN;
+	}
 
 	public void push(int i) {
 		if (last == null)
 			last = new Node(i);
 		else {
-			last.next = new Node(i);
-			last.next.prev = last;
-			last = last.next;
+			last.setNext(new Node(i));
+			last.getNext().setPrev(last);
+			last = last.getNext();
 		}
 	}
 
@@ -27,15 +31,22 @@ public class IntLinkedList {
 	public int top() {
 		if (isEmpty())
 			return DEFAULT_RETURN;
-		return last.value;
+		return last.getValue();
 	}
 
 	public int pop() {
 		if (isEmpty())
 			return DEFAULT_RETURN;
-		int ret = last.value;
-		last = last.prev;
+		int ret = last.getValue();
+		last = last.getPrev();
 		return ret;
 	}
 
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
 }
--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,12 +1,39 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ *  protected Node prev - previous list's node
+ *  protected Node next - next list's node
+ *
+ */
+
+
 public class Node {
 
-	final protected int value;
-	protected Node prev, next;
+	private final int value;
+	private Node prev;
+	private Node next;
 
 	public Node(int i) {
 		value = i;
 	}
 
+	public int getValue() {
+		return value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
 }
